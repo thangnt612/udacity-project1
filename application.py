a5c2c99befb32b2c3ef8e3bdda00671661e74496
THAVICO-JMFBEGR\Admin <thangnt612@gmail.com>
2022-07-29 10:03:18 +0700

    Update:library

--- application.py
+++ application.py
@@ -4,12 +4,15 @@ This script runs the FlaskWebProject application using a development server.
 
 from os import environ
 from FlaskWebProject import app
-
+HOST = 'localhost'
+PORT = 5555
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
+    print('-----------------------------'+HOST+'-----------------------------')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
+        print(PORT)
     except ValueError:
         PORT = 5555
-#app.run(HOST, PORT, ssl_context='adhoc')
-app.run(HOST, PORT, debug=True)
+app.run(HOST, PORT, ssl_context='adhoc')
+#app.run(HOST, PORT, debug=True)
