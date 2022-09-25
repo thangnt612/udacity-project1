8775061b19e94697a0d42bc6bf46cb9c9da840e5
THAVICO-JMFBEGR\Admin <thangnt612@gmail.com>
2022-09-25 12:29:58 +0700

    update

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class BaseConfig:
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
-    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
+    SECRET_KEY = 'abtPuk5sxWjHEJHM17eZSgI4Cp2V3nC97HEseklknQ0='
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://notificationqueue.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=abtPuk5sxWjHEJHM17eZSgI4Cp2V3nC97HEseklknQ0=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
